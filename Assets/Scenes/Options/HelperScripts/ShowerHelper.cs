06d74ddeb4541ea404df6843b62fa8e56b16b1a1
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2025-04-14 02:19:08 -0700

    dialogue stuff

--- Assets/Scenes/Options/HelperScripts/ShowerHelper.cs
+++ Assets/Scenes/Options/HelperScripts/ShowerHelper.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using Yarn.Unity;
 
 public class ShowerHelper : MonoBehaviour
 {
+    public DialogueRunner dialogueRunner;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(StatTracker.instance.hygeine < 0)
+        {
+            dialogueRunner.StartDialogue("ShowerBad");
+        }
+        else
+        {
+            StatTracker.instance.mood+=5;
+            StatTracker.instance.hygeine+=3;
+            dialogueRunner.StartDialogue("ShowerGood");
+        }
     }
 
     // Update is called once per frame
--- Assets/Scenes/Options/HelperScripts/SocialMediaHelper.cs
+++ Assets/Scenes/Options/HelperScripts/SocialMediaHelper.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using Yarn.Unity;
 
 public class SocialMediaHelper : MonoBehaviour
 {
+    public DialogueRunner dialogueRunner;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int option = Random.Range(1,4);
+        if(option == 1)
+        {
+            StatTracker.instance.charisma-=2;
+            StatTracker.instance.mood-=2;
+            dialogueRunner.StartDialogue("TwitterDialogue");
+        }
+        if(option == 2)
+        {
+            StatTracker.instance.dilligence-=1;
+            StatTracker.instance.mood+=4;
+            dialogueRunner.StartDialogue("RedditDialogue");
+        }
+        if(option ==3)
+        {
+            StatTracker.instance.dilligence+=3;
+            StatTracker.instance.mood-=2;
+            dialogueRunner.StartDialogue("YoutubeDialogue");
+        }
     }
 
     // Update is called once per frame
