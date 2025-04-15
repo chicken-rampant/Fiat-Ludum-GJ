bfafd7ed26b1a760a3190b9ae126203da64a8f48
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2025-04-14 17:28:07 -0700

    endings added

--- Assets/Scripts/StatTracker.cs
+++ Assets/Scripts/StatTracker.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using Unity.Properties;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StatTracker : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class StatTracker : MonoBehaviour
     public static StatTracker instance;
     [CreateProperty]
     public int daysElapsed, timeElapsed, mood, hygeine, dilligence, charisma, money, 
-    daysSinceGacha, daysSinceSlept, daysSinceCleaned;
+    daysSinceGacha, daysSinceSlept, daysSinceCleaned, daysSinceForcedGachaPull;
 
 
     public void resetStats()
@@ -35,67 +36,87 @@ public class StatTracker : MonoBehaviour
             daysSinceCleaned++;
             daysSinceGacha++;
             daysSinceSlept++;
+            daysSinceForcedGachaPull++;
         }
 
         if(daysElapsed%7==0)
         {
+            SceneManager.LoadSceneAsync("Allowance");
+            return;
             //trigger allowance
         }
 
         if(daysSinceCleaned>=5)
         {
+            daysSinceCleaned=0;
+            SceneManager.LoadSceneAsync("AsianMom");
+            return;
             //trigger mom angry scene
         }
 
         if(daysSinceSlept>=2)
         {
+            daysSinceSlept=1;
+            SceneManager.LoadSceneAsync("SleepDeprived");
+            return;
             //trigger sleep issue
         }
 
-        if(mood<=-20)
+        if(mood<=-20 &&daysSinceForcedGachaPull>=2)
         {
+            daysSinceForcedGachaPull=0;
+            SceneManager.LoadSceneAsync("StressGachaing");
+            return;
             //trigger forced gacha pull
         }
 
         if(daysElapsed > 50)
         {
+            SceneManager.LoadSceneAsync("Disowned");
+            return;
             //trigger mom kicks you out event
         }
 
         if(daysSinceGacha>=3)
         {
+            SceneManager.LoadSceneAsync("Withdrawals");
+            return;
             //die from gacha withdrawals
         }
 
         if(daysElapsed>=10&&hygeine<0)
         {
+            SceneManager.LoadSceneAsync("Cockroach");
+            return;
             //trigger cockroach infestation
         }
 
         if(mood<=-40)
         {
+            SceneManager.LoadSceneAsync("CreditCardDebt");
+            return;
             //gacha addict
         }
 
         //go outside & good gacha ending separate
 
-        if(daysElapsed>=15 && charisma<0)
-        {
-            //no aura death
-        }
-
         if(charisma>25&&mood>25&&dilligence>25&&hygeine>25)
         {
-            //isekai ending
+            SceneManager.LoadSceneAsync("Isekai");
+            return;
         }
 
         if(dilligence>40)
         {
+            SceneManager.LoadSceneAsync("FriesInTheBag");
+            return;
             //fries in the bag
         }
 
         if(hygeine>40)
         {
+            SceneManager.LoadSceneAsync("StayAtHomeSon");
+            return;
             //stay at home son
         }
     }
